3cdbff6136e95f78a21d7edd68089c532d326e43
TekilaXz <51700662+TekilaXz@users.noreply.github.com>
2019-06-11 13:47:30 +0200

    Add files via upload

---
+++ BotListener.java
@@ -0,0 +1,14 @@
+package fr.tekila.botdiscord;
+
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+public class BotListener implements EventListener {
+
+	@Override
+	public void onEvent(Event arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
